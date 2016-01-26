2d9dad61c599340104814a6c2b4e88591ded6c7c
Ilya Petrov <ilyaigpetrov@gmail.com>
2016-01-26 15:38:40 +0500

    update button added

--- extensions/chromium/minimalistic-pac-setter/extnesion/pages/choose-pac-provider/index.js
+++ extensions/chromium/minimalistic-pac-setter/extnesion/pages/choose-pac-provider/index.js
@@ -11,20 +11,48 @@ chrome.runtime.getBackgroundPage( backgroundPage => {
 
   var antiCensorRu = backgroundPage.antiCensorRu;
 
-  var ul = document.querySelector('#list-of-providers');
-  var _firstChild = ul.firstChild;
-  for( var providerKey of Object.keys(antiCensorRu.pacProviders) ) {
-    var li = document.createElement('li');
-    li.innerHTML = '<input type="radio" name="pacProvider" id="'+providerKey+'"> <label for="'+providerKey+'">'+providerKey+'</label>';
-    ul.insertBefore( li, _firstChild );
+  // DATE
+
+  var dateForUser = 'неизвестно';
+  if( antiCensorRu.lastPacUpdateStamp ) {
+    var diff = Date.now() - antiCensorRu.lastPacUpdateStamp;
+    var units = ' мс'
+    var gauges = [
+      [1000, ' с'],
+      [60, ' мин'],
+      [60, ' ч'],
+      [24, ' дн'],
+      [7, ' недель'],
+      [4, ' месяцев'],
+      [12, ' г']
+    ];
+    for(var g of gauges) {
+      var diffy = Math.floor(diff / g[0]);
+      if (!diffy)
+        break;
+      diff = diffy;
+      var units = g[1];
+    }
+    dateForUser = diff + units + ' назад';
   }
+
+  var dateElement = document.querySelector('.update-date');
+  dateElement.innerText = dateForUser;
+  dateElement.title = new Date(antiCensorRu.lastPacUpdateStamp).toLocaleString('ru-RU');
+
+  // CLOSE
+
+  document.querySelector('.close-button').onclick = () => window.close();
+
+  // RADIOS
 
   var currentRadio = () => {
     var id = antiCensorRu.currentPacProviderKey || 'none';
     return document.querySelector('#'+id);
   }
   var checkChosenProvider = () => {
-    currentRadio().checked = true;
+    var radio = currentRadio();
+    radio.checked = true;
   }
   var triggerChosenProvider = () => {
     var event = document.createEvent('HTMLEvents');
@@ -32,15 +60,25 @@ chrome.runtime.getBackgroundPage( backgroundPage => {
     currentRadio().dispatchEvent(event);
   }
 
+  var ul = document.querySelector('#list-of-providers');
+  var _firstChild = ul.firstChild;
+  for( var providerKey of Object.keys(antiCensorRu.pacProviders) ) {
+    var li = document.createElement('li');
+    li.innerHTML = '<input type="radio" name="pacProvider" id="'+providerKey+'"> <label for="'+providerKey+'">'+providerKey+'</label> <a href class="link-button">[обновить]</a>';
+    li.querySelector('.link-button').onclick = () => {triggerChosenProvider(); return false;};
+    ul.insertBefore( li, _firstChild );
+  }
+
   var radios = [].slice.apply( document.querySelectorAll('[name=pacProvider]') );
   for(var radio of radios) {
     radio.onchange = function(event) {
       var pacKey = event.target.id;
       if (pacKey === 'none')
-        return antiCensorRu.clearPac( () => window && window.close() );
+        return antiCensorRu.clearPac();
 
       function enableDisableInputs() {
-        var inputs = document.querySelectorAll('[name="pacProvider"]');
+        var inputs = document.querySelectorAll('input');
+        console.log(inputs.length);
         for (var i = 0; i < inputs.length; i++)
           inputs[i].disabled = !inputs[i].disabled;
       }
@@ -49,7 +87,7 @@ chrome.runtime.getBackgroundPage( backgroundPage => {
       setStatusTo('Установка...');
       antiCensorRu.installPac(pacKey, () => {
         setStatusTo('PAC-скрипт установлен.');
-        if(window) window.close();
+        enableDisableInputs();
       });
     }
   }
