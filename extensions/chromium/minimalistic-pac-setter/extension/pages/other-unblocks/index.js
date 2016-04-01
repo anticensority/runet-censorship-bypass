004ca1908714735cbe782c2a5bdd0fceb68e93dd
ilyaigpetrov <ilyaigpetrov+git@gmail.com>
2016-04-01 10:06:35 +0100

    modified context menu entries

--- extensions/chromium/minimalistic-pac-setter/extension/pages/other-unblocks/index.js
+++ extensions/chromium/minimalistic-pac-setter/extension/pages/other-unblocks/index.js
@@ -7,11 +7,11 @@ document.body.innerHTML = `
   <h3>Веб-прокси</h3>
     <a href="https://www.vpnbook.com/webproxy">vpnbook [COPY]</a><br/>
     <a href="https://webproxy.com/browse.php?u=${url}">webproxy.com</a><br/>
-    <a href="http://buka.link/browse.php?u=${url.replace(/\?.+/,'')}">buka.link [AVAST]</a><br/>
+    <a href="http://buka.link/browse.php?u=${url.replace(/\?.+/,'')}">buka.link [AVAST, COPY]</a><br/>
     <a href="https://hide.me/en/proxy">hide.me [COPY]</a><br/>
     <a href="https://www.hidemyass.com/proxy">Hide my ass [COPY]</a><br/>
     <a href="https://www.google.com/search?q=webproxy">Другие</a>
-  <h3>Из кеша поисковиков</h3>
+  <h3>Из кэша поисковиков</h3>
     <a href="https://webcache.googleusercontent.com/search?q=cache:${url}">Google</a></br>
     <a href="http://viewcached.com/${url}">viewcached.com</a></br>
     <a href="http://cachedview.com">cachedview.com [COPY]</a>
@@ -19,7 +19,22 @@ document.body.innerHTML = `
     <a href="http://web.archive.org/web/*/${url}">web.archive.org</a><br/>
     <a href="http://archive.is/${url}">archive.is</a><br/>
     <a href="http://timetravel.mementoweb.org">Momento Time Travel [COPY]</a>
+  <h3>Снимки страниц</h3>
+    <a href="https://screenshotmachine.com">Screenshot Machine [COPY]</a><br/>
+    <a href="https://www.url2png.com">url2png [COPY]</a><br/>
+    <a href="http://site2pic.com/">site2pic [COPY]</a><br/>
+    <a href="https://browshot.com">brow shot [COPY]</a>
+  <h3>Инструменты</h3>
+    <a href="http://isup.me/{url}">Сайт доступен из-за границы? isup.me</a><br/>
+    <a href='
+			data:text/html;charset=utf8,<title>Запрашиваю...</title>
+	    <form method="POST" action="https://www.host-tracker.com/ru/InstantCheck/Create">
+  	    <input name="InstantCheckUrl" value="${url}" type="hidden">
+	    </form>
+  	  <script>document.forms[0].submit()</script>
+		'>Сайт доступен из-за границы? host-tracker</a>
 `;
+
 var _ = document.querySelector('input');
 _.onfocus = function() { this.select() };
 document.querySelector('button').onclick = () => {_.focus(); document.execCommand('copy')};
