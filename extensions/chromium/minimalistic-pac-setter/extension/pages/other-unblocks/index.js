32ff64a247f9e69544d91854a85e856ed8773873
ilyaigpetrov <ilyaigpetrov@gmail.com>
2016-05-15 13:08:23 +0000

    Add Google DNS, change context menu, change code style, prepare 0.0.0.14 release images and text

--- extensions/chromium/minimalistic-pac-setter/extension/pages/other-unblocks/index.js
+++ extensions/chromium/minimalistic-pac-setter/extension/pages/other-unblocks/index.js
@@ -1,15 +1,17 @@
 'use strict';
 
-var url = decodeURIComponent(window.location.search.substring(1));
+const url = decodeURIComponent(window.location.search.substring(1));
 document.body.innerHTML = `
   <input type="text" value="${url}" autofocus style="width: 100%; border: 0; outline: none; font-size: 2em"/>
   <button>Копировать</button>
   <h3>Веб-прокси</h3>
     <a href="https://www.vpnbook.com/webproxy">vpnbook [COPY]</a><br/>
-    <a href="https://webproxy.com/browse.php?u=${url}">webproxy.com</a><br/>
+    <a href="https://webproxy.com/browse.php?u=${url}">webproxy.com [COPY]</a><br/>
     <a href="http://buka.link/browse.php?u=${url.replace(/\?.+/,'')}">buka.link [AVAST, COPY]</a><br/>
     <a href="https://hide.me/en/proxy">hide.me [COPY]</a><br/>
     <a href="https://www.hidemyass.com/proxy">Hide my ass [COPY]</a><br/>
+    <a href="http://usafastproxy.com">USA Fast Proxy [COPY, HTTP, GLYPE]</a><br/>
+    <a href="http://www.mysafesurfing.com">My Safe Surfing [COPY, HTTP, GLYPE]</a><br/>
     <a href="https://www.google.com/search?q=webproxy">Другие</a>
   <h3>Из кэша поисковиков</h3>
     <a href="https://webcache.googleusercontent.com/search?q=cache:${url}">Google</a></br>
@@ -35,6 +37,6 @@ document.body.innerHTML = `
 		'>Сайт доступен из-за границы? host-tracker</a>
 `;
 
-var _ = document.querySelector('input');
+const _ = document.querySelector('input');
 _.onfocus = function() { this.select() };
 document.querySelector('button').onclick = () => {_.focus(); document.execCommand('copy')};
