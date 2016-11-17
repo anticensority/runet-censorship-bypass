4872ef1c1e89efc0711c4b6f828d387c11e6d133
Ilya Ig. Petrov <ilyaigpetrov@gmail.com>
2016-11-17 10:44:48 -0800

    Replace internal pages with URLs on external

--- extensions/chromium/minimalistic-pac-setter/extension/3-context-menus.js
+++ extensions/chromium/minimalistic-pac-setter/extension/3-context-menus.js
@@ -17,54 +17,14 @@
 
   createMenuLinkEntry( 'Сайт доступен из-за границы? Is up?', (tab) => 'http://isup.me/'+ new URL(tab.url).hostname );
 
-  window.reestrUrl = 'http://reestr.rublacklist.net/search/?q=';
+  createMenuLinkEntry( 'Сайт в реестре блокировок?', (tab) => 'https://antizapret.info/index.php?search=' + tab.url );
 
-  createMenuLinkEntry( 'Сайт в реестре блокировок?', (tab) => {
-
-    const ifHost = confirm('Да  — искать по домену\nНет — искать по IP (зависит от местоположения)');
-    const hostname = new URL( tab.url ).hostname;
-    if (ifHost) {
-      return reestrUrl + hostname;
-    }
-    let ip = window.tabWithError2ip[tab.id];
-    if ( /^[.\d]+$/.test(hostname) || /^[:\dA-Fa-f]+$/.test(hostname) ) {
-      ip = hostname;
-    }
-    return ip
-      ? reestrUrl + ip
-      : chrome.extension.getURL('./pages/is-ip-blocked/index.html') +'?'+ hostname
-  });
-
-  createMenuLinkEntry( 'Из кэша Google', (tab) => 'https://webcache.googleusercontent.com/search?q=cache:' + removeProtocol(tab.url) );
+  createMenuLinkEntry( 'Из кэша Google', (tab) => 'http://webcache.googleusercontent.com/search?q=cache:' + removeProtocol(tab.url) );
 
   createMenuLinkEntry( 'Из архива archive.org', (tab) => 'https://web.archive.org/web/*/' + removeProtocol(tab.url) );
 
   createMenuLinkEntry( 'Открыть веб прокси (не наш)', (tab) => 'https://kproxy.com' );
 
-  createMenuLinkEntry( 'Другие варианты', (tab) => {
-
-    const ifPage = confirm('Да  — открыть страницу с решениями\nНет — сделать свежий снимок страницы (Screenshot Machine)');
-    return ifPage
-      ? chrome.extension.getURL('./pages/other-unblocks/index.html') + '?' + removeProtocol(tab.url)
-      : `data:text/html;charset=utf8,<title>Screenshot machine</title>
-            <form method="POST" action="https://screenshotmachine.com/processor.php">
-                <input name="urlparam" value="${ removeProtocol(tab.url) }" type="hidden">
-                <input name="size" value="F" type="hidden">
-                <input name="cacheLimit" value="0" type="hidden">
-            </form>
-            <script>document.forms[0].submit()</script>`;
-  });
-
-  /*
-  createMenuLinkEntry( 'Свежий снимок страницы, Screenshot machine', (tab) => `
-    data:text/html;charset=utf8,<title>Screenshot machine</title>
-        <form method="POST" action="https://screenshotmachine.com/processor.php">
-            <input name="urlparam" value="${ removeProtocol(tab.url) }" type="hidden">
-            <input name="size" value="F" type="hidden">
-            <input name="cacheLimit" value="0" type="hidden">
-        </form>
-        <script>document.forms[0].submit()</script>`
-  );
-  */
+  createMenuLinkEntry( 'Другие варианты разблокировки', (tab) => 'https://rebrand.ly/unblock#' + tab.url );
 
 }();
