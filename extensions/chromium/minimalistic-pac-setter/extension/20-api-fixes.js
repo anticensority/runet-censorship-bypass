0e04df6c45116de96e3f3d6c999f493dd161140c
Ilya Ig. Petrov <ilyaigpetrov@gmail.com>
2016-12-23 13:38:35 +0000

    Fix apis error context with timeout, change default locale, colorize icons

---
+++ extensions/chromium/minimalistic-pac-setter/extension/20-api-fixes.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/* `setTimeout` changes context of execution from other window
+    (e.g. popup) to background window, so we may catch errors
+    in bg error handlers.
+    More: https://bugs.chromium.org/p/chromium/issues/detail?id=357568
+    setTimeout is applied to Async methods only (name ends with Async)
+*/
+// Fix error context of methods of all APIs.
+
+for(const apiName of Object.keys(window.apis)) {
+  const api = window.apis[apiName];
+  for(const prop of Object.keys(api)) {
+    const method = api[prop];
+    if ( !(
+            typeof(api[prop]) === 'function'
+            && ['Async', 'Void'].some( (suff) => method.name.endsWith(suff) )
+          )
+    ) {
+      continue;
+    }
+    api[prop] = function(...args) {
+      setTimeout(method.bind(this, ...args), 0);
+    };
+  }
+}
