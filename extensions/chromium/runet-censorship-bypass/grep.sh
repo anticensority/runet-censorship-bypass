186ca1bdb4e05e184a42f187904e3085012a5a1f
ilyaigpetrov <ilyaigpetrov@gmail.com>
2019-05-04 09:35:08 -0500

    Make grep insensetive

--- extensions/chromium/runet-censorship-bypass/grep.sh
+++ extensions/chromium/runet-censorship-bypass/grep.sh
@@ -1,1 +1,1 @@
-grep -r "$@" ./*.js ./src --exclude-dir=vendor --exclude-dir=node_modules --exclude-dir=dist
+grep -ir "$@" ./*.js ./src --exclude-dir=vendor --exclude-dir=node_modules --exclude-dir=dist
